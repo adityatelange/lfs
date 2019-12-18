9be1bf377aa24b81bbc3410399a9fe95bfb75cbf
Aditya Telange <21258296+adityhere@users.noreply.github.com>
2019-12-18 23:11:49 +0530

    bison gwak m4 python3 makeinfo

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,5 +1,9 @@
 # adding symbolic link | fixes: ERROR: /bin/sh does not point to bash
 ln -sf bash /bin/sh 
 
-apt-get -y update 
+# Binutils, gcc , g++, xz, make, patch
+apt-get -y update > /dev/null 2>&1
 apt-get install -y --no-install-recommends build-essential
+
+# bison gwak m4 python3 makeinfo
+apt-get install -y --no-install-recommends bison gawk m4 python3 texinfo
